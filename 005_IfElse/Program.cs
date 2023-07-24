5ceaa92a706419feadc7d15cebf4f91aa520adc8
MakarovaElena <lena-chester@mai.ru>
2023-07-24 23:07:51 +0300

    005_IfElse/

---
+++ 005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лена")
+{
+    Console.WriteLine("Привет, дорогая Лена!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
