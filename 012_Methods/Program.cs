621f2baab48aac5a2f34ff2285880b4562bd408d
MakarovaElena <lena-chester@mai.ru>
2023-08-03 18:39:58 +0300

    012_Methods added

---
+++ 012_Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿// Вид 1
+void Method1()
+{
+    Console.WriteLine("Автор Макарова Елена");
+}
+// Method1();
+
+// Вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2(msg: "Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+
+}
+// Method21(msg: "Текст",count: 4);
+// Method21(count: 4,msg: "Новый текст");
+
+// Вид 3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+// 
+
+// Вид 4
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "z"); // в скобках указан текст, который будет подряд 10 раз
+Console.WriteLine(res);
